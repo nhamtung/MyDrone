2490355f266d29b08e106a67f721a66101adf8f5
nhamtung <nhamtung125@gmail.com>
2020-05-11 09:26:29 +0700

    create catkin_ws

---
+++ DetectWaterSurface/water-detection/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nhamtung/TungNV/MyDrone/DetectWaterSurface/water-detection/catkin_ws/src
+# Build directory: /home/nhamtung/TungNV/MyDrone/DetectWaterSurface/water-detection/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
